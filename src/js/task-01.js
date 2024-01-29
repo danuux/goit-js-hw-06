33fc71ac43c1747c3eed4f8d493aa50f9a1931c4
danuux <danut_laurentiu98@yahoo.com>
2024-01-29 20:06:46 +0200

    Initial push and first exercise

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const categoriesList = document.getElementById('categories');
+const categoryItem = categoriesList.querySelectorAll('.item');
+console.log('Number of categories:', categoryItem.length);
+
+// Iterare
+
+categoryItem.forEach
