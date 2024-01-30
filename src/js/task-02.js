9e2c9172f7246a100c659f98bcbb9f682b1d9d02
danuux <danut_laurentiu98@yahoo.com>
2024-01-30 20:09:46 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+ingredients.forEach(ingredient => {
+  const list = document.createElement('li');
+  list.textContent = ingredient;
+  list.classList.add('.item');
+  ingredientsList.append(list);
+});
